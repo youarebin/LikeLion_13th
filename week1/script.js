d1a224592bbb22616725d2ec3d1173678fa2d13e
kayubin <kayubin@gmail.com>
2025-03-12 20:36:33 +0900

    feat: 1주차 과제

---
+++ week1/script.js
@@ -0,0 +1,8 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the page:', error));
+}
